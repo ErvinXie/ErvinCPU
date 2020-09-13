b03b8477444cd758c556ff20de4071bee508159f
ErvinXie <ervinxie@foxmail.com>
2020-09-13 16:20:52 +0800

    docs ok

--- test/fib.asm
+++ test/fib.asm
@@ -4,28 +4,20 @@
 
 .text
 main:
-	
-	addiu $t1,$0,1
+	li $t1,1
 	li $t2,1
-
 	li  $t4,0
 	lw $t5,loopcnt
-	
-	
 	la  $t6,array
-
 loop:
 	add		$t0, $t1, $t2		# $t0 = $t1 + $t2
 	move 	$t2, $t1		# $t2 = $t1
 	move 	$t1, $t0		# $t1 = $01
 	sw		$t1, 0($t6)		
-	
 	addiu   $t4,$t4,1
 	addiu   $t6,$t6,4
-
 	beq		$t4, $t5, exit	# if $t4 == 5t1 then target
 	j		loop				# jump to loop
-
 exit:
 	divu	$t1,$t2
 
